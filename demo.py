93cad0f65494e8a4649cbc807d4ad0e2cadbec08
yangjie11 <yangjie11@baidu.com>
2017-09-04 17:33:32 +0800

    hello world demo

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, Workd")
 
-    def make_app(self):
+    def make_app():
         return tornado.web.Application([(r"/", MainHandler),])
 
     if __name__ == "__main__":
