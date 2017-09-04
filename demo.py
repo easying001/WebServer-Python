51708328cfe25b2e159f015ce725254ede47f6a9
yangjie11 <yangjie11@baidu.com>
2017-09-04 17:27:21 +0800

    添加hello world demo

---
+++ demo.py
@@ -0,0 +1,14 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, Workd")
+
+    def make_app(self):
+        return tornado.web.Application([(r"/", MainHandler),])
+
+    if __name__ == "__main__":
+        app = make_app()
+        app.listen(8888)
+        tornado.ioloop.IOLoop.current().start();
