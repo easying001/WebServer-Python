54c6566afeb534f1279a411a5172fe00bbe5ed08
yangjie11 <yangjie11@baidu.com>
2017-09-04 17:43:42 +0800

    hello world demo

--- demo.py
+++ demo.py
@@ -1,14 +1,27 @@
+import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, Workd")
+        self.write("Hello, world")
 
-def make_app():
-    return tornado.web.Application([(r"/", MainHandler),])
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start();
+    main()
