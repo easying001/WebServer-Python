4b366daa36b4d28d490390aaea3f21eac59d1fc6
yangjie11 <yangjie11@baidu.com>
2017-09-04 17:35:24 +0800

    hello world demo

--- demo.py
+++ demo.py
@@ -5,10 +5,10 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, Workd")
 
-    def make_app():
-        return tornado.web.Application([(r"/", MainHandler),])
+def make_app():
+    return tornado.web.Application([(r"/", MainHandler),])
 
-    if __name__ == "__main__":
-        app = make_app()
-        app.listen(8888)
-        tornado.ioloop.IOLoop.current().start();
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start();
