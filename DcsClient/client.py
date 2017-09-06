3e3297d5a728d2d633996e41c135a256cd286b9a
yangjie11 <yangjie11@baidu.com>
2017-09-06 20:59:04 +0800

    upload user's speech to dcs server

--- DcsClient/client.py
+++ DcsClient/client.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
         httpConn.send(json_part.encode('utf-8'), final=False, stream_id=stream_id)
 
         resp = httpConn.get_response(stream_id)
-        print resp
+        print resp.headers
 
         done = True
 
